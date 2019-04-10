863c824fe2ae7a07aa827c28fa5895a767945c5a
Michele Federici @ps1dr3x <michele@federici.tech>
2019-04-10 14:51:31 +0200

    createEscrow first draft

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-/* global $ */
+/* global $, Neon */
+
+const CONTRACT_ADDRESS = ''
+const CONTRACT_SCRIPTHASH = ''
+const PRIV_RPC_NODE = ''
 
 let pkey = ''
+let amount = 0
+let sellerAddress = ''
 
 $('.login-form button').click(() => {
   $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow')
@@ -8,5 +14,32 @@ $('.login-form button').click(() => {
 })
 
 $('.create-escrow-form button').click(() => {
-  console.log(pkey)
+  amount = $('.login-form input').val()
+  sellerAddress = $('.login-form input').val()
+  createEscrow(pkey, amount, sellerAddress)
 })
+
+function createEscrow (pkey, amount, sellerAddress) {
+  const apiProvider = new Neon.api.neoscan.instance('PrivateNet')
+
+  const intent = Neon.api.makeIntent({ NEO: amount }, CONTRACT_ADDRESS)
+  const props = {
+    scriptHash: CONTRACT_SCRIPTHASH,
+    operation: 'create_escrow',
+    args: [sellerAddress]
+  }
+  const script = Neon.default.create.script(props)
+  const config = {
+    api: apiProvider,
+    url: PRIV_RPC_NODE,
+    account: pkey,
+    intents: intent,
+    script: script
+  }
+
+  Neon.default.doInvoke(config).then(config => {
+    console.log(config.response)
+  }).catch(config => {
+    console.log(config)
+  })
+}
